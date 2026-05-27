4fb09767c97567c029f187d852ae98ab6ff5b700
itc-n26014 <n26014@std.it-college.ac.jp>
2026-05-27 07:51:11 +0000

    例題13から26まで

---
+++ Chapter03/ex3-25.py
@@ -0,0 +1,11 @@
+home = "火星"
+if home == "アメリカ":
+    print("Hello,America!")
+elif home == "カナダ":
+    print("Hello,Camada!")
+elif home == "タイ":
+    print("Hello,Tailand!")
+elif home == "メキシコ":
+    print("Hello,Mexico!")
+else:
+    print("Hello,World!")
