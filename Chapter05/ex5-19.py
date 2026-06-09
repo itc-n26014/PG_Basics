476e08d0e1a59ad13e6016db582abf49007b24f5
itc-n26014 <n26014@std.it-college.ac.jp>
2026-06-09 07:57:36 +0000

    第5章　例題19まで終了

---
+++ Chapter05/ex5-19.py
@@ -0,0 +1,16 @@
+facts = dict()
+
+
+#バリューを追加
+facts["code"] = "fun"
+#キーを参照
+print(facts["code"])
+#バリューを追加
+facts["Bill"] = "Gates"
+#キーを参照
+print(facts["Bill"])
+#バリューを追加
+facts["founded"] = 1776
+#キーを参照
+print(facts["founded"])
+
